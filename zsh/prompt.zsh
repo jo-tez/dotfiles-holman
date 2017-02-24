936fbdd50d6f24e8fcb1a8e201a23d25b1ac5174
Johan Tez <jtesznar@gmail.com>
2017-02-24 15:38:31 +0000

    Remove battery status

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -54,11 +54,8 @@ directory_name() {
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-battery_status() {
-  $ZSH/bin/battery-status
-}
+export PROMPT=$'\n$(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
 
-export PROMPT=$'\n$(battery_status)in $(directory_name) $(git_dirty)$(need_push)\n› '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
